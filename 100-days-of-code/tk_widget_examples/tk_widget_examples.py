f013a764f66003a6d79a600839bff4f1a488126a
Eddy C. Pagan <ecpagan+git@gmail.com>
2023-02-14 00:39:21 -0400

    Tkinter widgets examples

---
+++ 100-days-of-code/tk_widget_examples/tk_widget_examples.py
@@ -0,0 +1,101 @@
+import tkinter
+
+# Creating a new window and configurations
+window = tkinter.Tk()
+window.title('Widget Examples')
+window.minsize(width=500, height=500)
+
+# Labels
+my_label = tkinter.Label(text='This is old text')
+my_label.config(text='This is new text')
+my_label.pack()  # to place it on the screen, by default is in the center
+
+
+# Buttons
+def button_clicked():
+    print('Button Clicked')
+
+
+# calls function when pressed
+button = tkinter.Button(text='Click Me', command=button_clicked)
+button.pack()
+
+# Entries
+entry = tkinter.Entry(width=30)
+# Add some text to begin with
+entry.insert(tkinter.END, string='Some text to begin with.')
+# Gets text in entry
+print(entry.get())
+entry.pack()
+
+# Text
+text = tkinter.Text(height=5, width=30)
+# Puts cursor in textbox
+text.focus()
+# Adds some text to begin with.
+text.insert(tkinter.END, 'Example of multi-line text entry.')
+# Gets current value in textbox at line 1, character 0
+print(text.get('1.0', tkinter.END))
+text.pack()
+
+
+# Spinbox
+def spinbox_used():
+    # gets the current value in spinbox
+    print(spinbox.get())
+
+
+spinbox = tkinter.Spinbox(from_=0, to=10, width=5, command=spinbox_used)
+spinbox.pack()
+
+
+# Scale
+# Called with current scale value
+def scale_used(value):
+    print(value)
+
+
+scale = tkinter.Scale(from_=0, to=100, command=scale_used)
+scale.pack()
+
+
+# Check Button
+def checkbutton_used():
+    # Prints 1 if On button checked, otherwise 0
+    print(checked_state.get())
+
+
+# variable to hold on to check state, 0 is off, 1 is on
+checked_state = tkinter.IntVar()
+checkbutton = tkinter.Checkbutton(text='Is On?', variable=checked_state, command=checkbutton_used)
+checked_state.get()
+checkbutton.pack()
+
+
+# Radio Button
+def radio_used():
+    print(radio_state.get())
+
+
+# Variable to hold on to which radio button value is checked
+radio_state = tkinter.IntVar()
+radiobutton1 = tkinter.Radiobutton(text='Option1', value=1, variable=radio_state, command=radio_used)
+radiobutton2 = tkinter.Radiobutton(text='Option2', value=2, variable=radio_state, command=radio_used)
+radiobutton1.pack()
+radiobutton2.pack()
+
+
+# Listbox
+def listbox_used(event):
+    # Gets current selection from listbox
+    print(listbox.get(listbox.curselection()))
+
+
+listbox = tkinter.Listbox(height=4)
+fruits = ['Apple', 'Pear', 'Orange', 'Banana']
+for item in fruits:
+    listbox.insert(fruits.index(item), item)
+listbox.bind('<<ListboxSelect>>', listbox_used)
+listbox.pack()
+
+window.mainloop()  # should be at the very end of the script, to hold the GUI
